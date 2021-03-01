eb41484a49adb056e7fef47af32d20911940070f
AlKharkov <79756667+AlKharkov@users.noreply.github.com>
2021-03-01 18:58:06 +0700

    Delete solution (1).py

--- solution (1).py
+++
@@ -1,8 +0,0 @@
-examples = list()
-with open('in_file.txt', mode='r') as f:
-    for line in f:
-        line = line.rstrip()
-        examples.append(f'{line} = {eval(line)}')
-with open('out_file.txt', mode='w') as f:
-    for example in examples:
-        f.write(example + '\n')
