2ad5a62f3ed0031d46c577c6133972c7dc1751ab
AlKharkov <79756667+AlKharkov@users.noreply.github.com>
2021-03-01 18:58:16 +0700

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-def creators(star, planet=8, alive=True):
-    n = "ь есть!"
-    if not alive:
-        n = "и нет."
-        return "Система звезды " + star + ".\nКоличество планет: " + str(planet) + ".\nЖизн" + n
